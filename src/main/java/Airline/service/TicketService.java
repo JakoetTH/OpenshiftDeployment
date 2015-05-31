5b8914fe6b1c2c2e9d93efbcc6d520c04a6c5e15
Thawhir Jakoet <thawhirwow@gmail.com>
2015-05-31 16:55:48 +0200

    small rename

--- src/main/java/Airline/service/TicketService.java
+++ src/main/java/Airline/service/TicketService.java
@@ -8,6 +8,6 @@ import java.util.List;
  * Created by student on 2015/05/24.
  */
 public interface TicketService {
-    public Iterable<Ticket> getPassengerTickets(String ID);
+    public List<Ticket> getPassengerTickets(String ID);
 
 }
