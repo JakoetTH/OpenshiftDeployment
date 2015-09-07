9e09048451992b1321311fd993a34e97a3c753a9
JakoetTH <thawhirwow@gmail.com>
2015-09-07 22:55:08 +0200

    new ticket services added

--- src/main/java/Airline/service/TicketImpl.java
+++ src/main/java/Airline/service/TicketImpl.java
@@ -44,12 +44,49 @@ public class TicketImpl implements TicketService {
         Flight updatedFlight = new Flight
                                         .Builder(flight.getDepartureLocation())
                                         .copy(flight)
-                                        .seatsAvailable(flight.getSeatsAvailable()-1)
+                                        .seatsAvailable(flight.getSeatsAvailable() - 1)
                                         .tickets(ftickets)
                                         .build();
 
         repositoryPassenger.save(updatedPassenger);
         repositoryFlight.save(updatedFlight);
         repositoryTicket.save(ticket);
+    }
+    @Override
+    public void deleteTicket(Ticket ticket)
+    {
+        Iterable<Passenger> passengers = repositoryPassenger.findAll();
+        Iterable<Flight> flights = repositoryFlight.findAll();
+        List<Ticket> ptickets;
+        List<Ticket> ftickets;
+        //Iterates through passenger objects and deletes ticket from existing passenger
+        for(Passenger pas : passengers)
+        {
+            ptickets = pas.getTickets();
+                if(ptickets.contains(ticket))
+                {
+                    ptickets.remove(ticket);
+                    pas = new Passenger
+                                    .Builder(pas.getUserName())
+                                    .copy(pas)
+                                    .tickets(ptickets).build();
+                    repositoryPassenger.save(pas);
+                }
+        }
+        //Iterates through flight objects and deletes ticket from existing flight
+        for(Flight fli : flights)
+        {
+            ftickets = fli.getTickets();
+                if(ftickets.contains(ticket))
+                {
+                    ftickets.remove(ticket);
+                    fli = new Flight
+                                .Builder(fli.getDepartureLocation())
+                                .copy(fli)
+                                .tickets(ftickets).build();
+                    repositoryFlight.save(fli);
+                }
+        }
+        repositoryTicket.delete(ticket);
     }
 }
