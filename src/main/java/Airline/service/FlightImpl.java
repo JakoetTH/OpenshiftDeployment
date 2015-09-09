8ed50bd91900bf1c32624729ade8dfe1bb23039d
JakoetTH <thawhirwow@gmail.com>
2015-09-09 21:02:34 +0200

    added additional flight service

--- src/main/java/Airline/service/FlightImpl.java
+++ src/main/java/Airline/service/FlightImpl.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Airline.domain.Flight;
+import Airline.domain.Passenger;
+import Airline.domain.Ticket;
 import Airline.repository.FlightRepository;
 
 @Service
@@ -20,6 +22,24 @@ public class FlightImpl implements FlightService {
         List<Flight> flights = new ArrayList<Flight>();
         for(Flight fli : iflights)
             flights.add(fli);
+        return flights;
+    }
+    @Override//Read (Most inefficient code ever)
+    public List<Flight> getAllPassengersFlights(Passenger pas)
+    {
+        Iterable<Flight> iflights = repositoryFlight.findAll();
+        List<Flight> flights = new ArrayList<Flight>();
+        List<Ticket> ptickets = pas.getTickets();
+        List<Ticket> ftickets;
+            for(Flight fli : iflights)
+            {
+                ftickets = fli.getTickets();
+                for(Ticket ticket : ptickets)
+                {
+                    if(ftickets.contains(ticket))
+                        flights.add(fli);
+                }
+            }
         return flights;
     }
 }
--- src/main/java/Airline/service/FlightService.java
+++ src/main/java/Airline/service/FlightService.java
@@ -4,7 +4,9 @@ package Airline.service;
 import java.util.List;
 
 import Airline.domain.Flight;
+import Airline.domain.Passenger;
 
 public interface FlightService {
     public List<Flight> getAllFlights();
+    public List<Flight> getAllPassengersFlights(Passenger pas);
 }
