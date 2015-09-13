71ea5046280ed1556f65ab3e925532de2b52dfeb
JakoetTH <thawhirwow@gmail.com>
2015-09-13 17:52:42 +0200

    save commit

--- src/main/java/Airline/api/PassengerController.java
+++ src/main/java/Airline/api/PassengerController.java
@@ -14,9 +14,18 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.lang.reflect.MalformedParametersException;
+import java.util.List;
+
+import Airline.domain.Flight;
 import Airline.domain.Passenger;
+import Airline.domain.Ticket;
+import Airline.repository.FlightRepository;
 import Airline.repository.PassengerRepository;
+import Airline.repository.TicketRepository;
+import Airline.service.FlightService;
 import Airline.service.PassengerService;
+import Airline.service.TicketService;
 
 @RestController
 @RequestMapping("/api/**")
@@ -24,7 +33,15 @@ public class PassengerController {
     @Autowired
     PassengerService passengerService;
     @Autowired
+    FlightService flightService;
+    @Autowired
+    TicketService ticketService;
+    @Autowired
     PassengerRepository passengerRepository;
+    @Autowired
+    TicketRepository ticketRepository;
+    @Autowired
+    FlightRepository flightRepository;
    // @RequestMapping(value = "/passenger/create",method = RequestMethod.POST)
    // public ResponseEntity<Void> createPassenger(@RequestBody Passenger passenger, UriComponentsBuilder ucBuilder)
    // {
@@ -45,12 +62,59 @@ public class PassengerController {
      //   headers.setLocation(ucBuilder.path("/subject/{id}").buildAndExpand(subject.getId()).toUri());
      //   return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
   //  }
-    @RequestMapping(value = "/passenger/{id}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Passenger> getPassenger(@PathVariable("id") long id)
+    //Creates new Ticket
+    @RequestMapping(value = "/ticket/create", method = RequestMethod.POST)
+    public ResponseEntity<Void> newTicket(@RequestParam long userid,
+                                          @RequestParam long flightid,
+                                          @RequestParam float price,
+                                          @RequestParam String ticketclass)
     {
-        Passenger passenger = passengerRepository.findOne(id);
+        Passenger passenger = passengerRepository.findOne(userid);
+        Flight flight = flightRepository.findOne(flightid);
+        ticketService.newTicket(passenger,flight,price,ticketclass);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+
+    //Delete a ticket by ticket ID
+    @RequestMapping(value = "/ticket/delete/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Ticket> deleteTicket(@PathVariable("id") long id)
+    {
+        Ticket ticket = ticketRepository.findOne(id);
+        if(ticket==null)
+            return new ResponseEntity<Ticket>(HttpStatus.NOT_FOUND);
+        ticketService.deleteTicket(id);
+        return new ResponseEntity<Ticket>(HttpStatus.NO_CONTENT);
+    }
+
+    //Returns all flights
+    @RequestMapping(value = "/flight/all",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Flight>> getAllFlights()
+    {
+        List<Flight> flights = flightService.getAllFlights();
+        if(flights==null)
+            return new ResponseEntity<List<Flight>>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<List<Flight>>(flights, HttpStatus.OK);
+    }
+
+    //Returns all of a passenger's flights
+    @RequestMapping(value = "/flight/bypassenger/{username}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Flight>> getAllPassengerFlights(@PathVariable("username") String username)
+    {
+        Passenger passenger = passengerService.getPassenger(username);
+        List<Flight> flights = flightService.getAllPassengersFlights(passenger);
+        if(flights==null)
+            return new ResponseEntity<List<Flight>>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<List<Flight>>(flights, HttpStatus.OK);
+    }
+
+    //Returns passenger by User name
+    @RequestMapping(value = "/passenger/{username}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Passenger> getPassenger(@PathVariable("username") String username)
+    {
+
+        Passenger passenger = passengerService.getPassenger(username);
         if(passenger==null)
-            return new ResponseEntity<Passenger>(HttpStatus.FOUND);
+            return new ResponseEntity<Passenger>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<Passenger>(passenger, HttpStatus.OK);
     }
 }
--- src/main/java/Airline/service/PassengerService.java
+++ src/main/java/Airline/service/PassengerService.java
@@ -6,6 +6,6 @@ import Airline.domain.Passenger;
  * Created by Thawhir on 2015/08/28.
  */
 public interface PassengerService {
-    public Passenger getPassenger(String ID);
+    public Passenger getPassenger(String username);
     public void newPassenger(String username, String password, String firstname, String lastname, String address, String contact);
 }
